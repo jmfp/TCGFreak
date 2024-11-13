0b92d454919b1593d5b22126c598c3a23a940dbf
jmfp <therealjmfp@gmail.com>
2024-11-13 17:42:09 -0500

    new dependencies

--- app/jobs.tsx
+++ app/jobs.tsx
@@ -129,19 +129,6 @@ const Jobs = () => {
         </View>
       </Animated.View>
       <Animated.View className="bg-slate-800 h-24 w-[80%] rounded-lg m-auto my-6 border-2 border-green-400" entering={FadeIn.delay(300).springify()} exiting={FadeOut}>
-        <Text className='text-white m-auto'>{testJob.title}</Text>
-        <Text className='text-white m-auto'>{testJob.description}</Text>
-        <Text className='text-white m-auto'>
-          Payment 
-          <Text className='text-green-400 m-auto'> ${testJob.price}</Text>
-        </Text>
-        <View className="bg-green-400">
-          <TouchableOpacity className="text-white text-center m-auto size-full">
-            Apply
-          </TouchableOpacity>
-        </View>
-      </Animated.View>
-      <Animated.View className="bg-slate-800 h-24 w-[80%] rounded-lg m-auto my-6 border-2 border-green-400" entering={FadeIn.delay(300).springify()} exiting={FadeOut}>
         <Text className='text-white m-auto text-4xl'>{testJob.title}</Text>
         <Text className='text-white m-auto text-2xl'>{testJob.description}</Text>
         <Text className='text-white m-auto'>
